24637b22e4ea6dbf45861726a33a1c5bdc2d9b89
ChenLin12138 <scorpion_chenlin@163.com>
2019-08-27 21:21:41 +0800

    添加@RefreshScipt注解

--- src/main/java/com/chenlin/licenseservice/LicenseingserviceApplication.java
+++ src/main/java/com/chenlin/licenseservice/LicenseingserviceApplication.java
@@ -2,8 +2,13 @@ package com.chenlin.licenseservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+//访问http://<yourserver>:8080/refersh端点可以刷新spring自定义的属性
+//自定义属性例如我们在serverConfig类中定义的${example.property}"
+//但是不会刷新spring data定义的数据库配置
+//@RefreshScope
 public class LicenseingserviceApplication {
 
 	public static void main(String[] args) {
