e8402b69dbb52935b04060687ca6379765f155b1
ChenLin12138 <scorpion_chenlin@163.com>
2019-09-03 21:45:27 +0800

    LicenseServer添加eureka client配置

--- src/main/java/com/chenlin/licenseservice/LicenseingserviceApplication.java
+++ src/main/java/com/chenlin/licenseservice/LicenseingserviceApplication.java
@@ -3,7 +3,6 @@ package com.chenlin.licenseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //访问http://<yourserver>:8080/refersh端点可以刷新spring自定义的属性
@@ -12,6 +11,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //@RefreshScope
 //使得Springboot可以使用DiscoveryClient和Ribbon库
 @EnableDiscoveryClient
+//此注解可以注释掉，springboot会根据classPath包含
+//spring-cloud-starter-netflix-eureka-client自动以eureka client向
+//eureka server注册改服务。
+//@EnableEurekaClient
 public class LicenseingserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LicenseingserviceApplication.class, args);
